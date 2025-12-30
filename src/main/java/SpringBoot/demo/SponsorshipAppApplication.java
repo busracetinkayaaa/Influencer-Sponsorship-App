7f084b7250e3ede138ef6ef14bee0a67031ba55e
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2025-12-31 02:45:49 +0300

    First commit

---
+++ src/main/java/SpringBoot/demo/SponsorshipAppApplication.java
@@ -0,0 +1,13 @@
+package SpringBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsorshipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsorshipAppApplication.class, args);
+	}
+
+}
